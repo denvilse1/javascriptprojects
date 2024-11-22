d94214c81708525c380ef9e36e8b0fd347c73850
denvilse1 <119152406+denvilse1@users.noreply.github.com>
2024-11-22 12:43:31 +0530

    things are coming

--- projectmoviefranchise/script.js
+++ projectmoviefranchise/script.js
@@ -27,3 +27,18 @@
 const title = document.querySelector("#main-heading");
 title.style.color = "red";
 console.log(title);
+
+const listname = document.querySelectorAll(".list-name");
+console.log(listname);
+
+for (i = 0; i < listname.length; i++) {
+  listname[i].style.fontSize = "2rem";
+}
+
+//!Creating elements
+
+const ul = document.querySelector("ul");
+const li = document.createElement("li");
+//? The above element will not do anything
+
+ul.append(li); // it's working
