30629b47339bc83a61668249520f652cca5af753
denvilse1 <119152406+denvilse1@users.noreply.github.com>
2024-11-27 14:27:14 +0530

    EVENT LISTENERS

--- evenelisternerproject/script.js
+++ evenelisternerproject/script.js
@@ -2,8 +2,8 @@
 
 //? element>addEventListener("click",funciton);
 
-const buttontwo = document.querySelector(".btn2");
-const buttonthree = document.querySelector(".btn3");
+const buttontwo = document.querySelector(".btn-1");
+const buttonthree = document.querySelector(".btn-3");
 
 //creating a call back function
 function alertBtn() {
@@ -15,3 +15,22 @@ function alertBtn() {
 buttontwo.addEventListener("click", alertBtn);
 
 buttonthree.addEventListener("click", alertBtn);
+
+//!MouseOver event
+
+const newBackgroundColor = document.querySelector(".btn3");
+
+function changeBgColor() {
+  if (newBackgroundColor) {
+    // Check if an element was found
+    function changeBgColor() {
+      newBackgroundColor.style.backgroundColor = "blue";
+    }
+
+    newBackgroundColor.addEventListener("mouseover", changeBgColor);
+  } else {
+    console.warn("No element with class '.btn3' found.");
+  }
+}
+
+newBackgroundColor.addEventListener("click", changeBgColor);
